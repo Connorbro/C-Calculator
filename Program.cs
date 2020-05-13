dc83f8bd3ba0e483f744ca05837e368bae986600
connor <vbrookesy@hotmail.co.uk>
2020-05-13 12:48:43 +0100

    Converting the users answer from a string to a double

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
         }
     }
 }
